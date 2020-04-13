0930b4d62be2996fa4e0198e50bec0bf5c31c1a9
Tawfiq Hamid <tawfiq.hamid@gmail.com>
2020-04-13 16:48:00 +0100

    Got emoji picker working; needs a lot of cleanup.

--- src/emoji-picker-app/emoji-picker-app.js
+++ src/emoji-picker-app/emoji-picker-app.js
@@ -1,5 +1,9 @@
 import {html, PolymerElement} from '@polymer/polymer/polymer-element.js';
 import '@polymer/paper-input/paper-input.js';
+import './emoji-selector.js';
+
+// var emoji = import('emoji-selector/emoji-selector.js');
+// document.registerElement('emoji-selector', { prototype: emoji });
 
 /**
  * @customElement
@@ -17,6 +21,7 @@ class EmojiPickerApp extends PolymerElement {
       <h2>Search for an emoji:</h2>
       <paper-input value="{{search_string}}"></paper-input>
       <p>Currently searching for:[[search_string]]</p>
+      <emoji-selector search-string="{{search_string}}"></emoji-selector>
     `;
   }
   static get properties() {
